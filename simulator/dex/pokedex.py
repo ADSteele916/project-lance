62828f2143d4cddf75ac7cb7af90ba84e23b938b
Alex Steele <45648397+ADSteele916@users.noreply.github.com>
2022-08-28 18:06:12 -0700

    Refactor pokedex generation

--- simulator/dex/pokedex.py
+++ simulator/dex/pokedex.py
@@ -7,23 +7,28 @@ from simulator.dex.movedex import MOVEDEX
 from simulator.pokemon.pokemon_species import PokemonSpecies
 from simulator.type import Type
 
-with open(os.path.join(os.path.dirname(__file__), "pokedex.json")) as json_file:
-    __dex_dict_list = json.load(json_file)
 
-POKEDEX = {}
+def _gen_pokedex():
+    with open(os.path.join(os.path.dirname(__file__), "pokedex.json")) as json_file:
+        dex_dict_list = json.load(json_file)
 
-for __pokemon in __dex_dict_list:
-    POKEDEX[__pokemon["name"]] = PokemonSpecies(
-            __pokemon["name"],
-            __pokemon["dex_num"],
-            __pokemon["base_hp"],
-            __pokemon["base_atk"],
-            __pokemon["base_def"],
-            __pokemon["base_spe"],
-            __pokemon["base_spc"],
-            set(map(lambda move: MOVEDEX[move],
-                    filter(lambda move: move in MOVEDEX,
-                           __pokemon["learnset"]))),
-            Type[__pokemon["primary_type"].upper()],
-            None if __pokemon["secondary_type"] is None else Type[__pokemon["secondary_type"].upper()],
-    )
+    pokedex = {}
+
+    for pokemon in dex_dict_list:
+        pokedex[pokemon["name"]] = PokemonSpecies(
+                pokemon["name"],
+                pokemon["dex_num"],
+                pokemon["base_hp"],
+                pokemon["base_atk"],
+                pokemon["base_def"],
+                pokemon["base_spe"],
+                pokemon["base_spc"],
+                set(MOVEDEX[move] for move in pokemon["learnset"] if move in MOVEDEX),
+                Type[pokemon["primary_type"].upper()],
+                None if pokemon["secondary_type"] is None else Type[pokemon["secondary_type"].upper()],
+        )
+
+    return pokedex
+
+
+POKEDEX = _gen_pokedex()
